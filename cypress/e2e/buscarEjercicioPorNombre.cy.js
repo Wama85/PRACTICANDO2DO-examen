dcd7c3dfd3743aa7082bee4a8c90efe7117d7981
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-11-11 20:37:13 -0400

    Verifica que la imagen sea visible de las coincidencias del ejercicio buscado

--- cypress/e2e/buscarEjercicioPorNombre.cy.js
+++ cypress/e2e/buscarEjercicioPorNombre.cy.js
@@ -15,5 +15,14 @@ describe("Buscar ejecicio por nombre", () => {
         cy.get("#contenidodetallecatalogo p").should("contain","Los números primos son enteros mayores que 1 con solo dos divisores, 1 y ellos mismos. No son divisibles por ningún otro número. Son fundamentales en matemáticas y tienen aplicaciones en criptografía y ciencias de la computación.Ver mas");
   
       });
+
+      it("Verifica que la imagen sea visible de las coincidencias del ejercicio buscado", () => {
+        cy.visit("/");
+        cy.get("#menubuscarejer").click();
+        cy.get("#txtbuscar").type("Numeros Primos");
+        cy.get("#txtbuscar").type("{enter}");
+        cy.get("#marcoimagen img").should('be.visible');
+  
+      });
   });
 
