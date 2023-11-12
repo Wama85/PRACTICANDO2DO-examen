b07efa14ef110f0c47e87c7634e3e3b68c051166
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-11 22:39:16 -0400

    Refactoring pruebas

--- cypress/e2e/crearUsuario.cy.js
+++ cypress/e2e/crearUsuario.cy.js
@@ -1,20 +1,17 @@
 describe("Crear usuario", () => {
-
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("#menucrearusuario").click();
+  });
     it("Registrar nombre de usuario", () => {
-      cy.visit("/");
-      cy.get("#menucrearusuario").click();
       cy.get("#txtusername").type("Perro salchicha"); 
       cy.get("#txtusername").should('have.value','Perro salchicha'); 
     });
     it("Registrar password de usuario", () => {
-        cy.visit("/");
-        cy.get("#menucrearusuario").click();
         cy.get("#txtpassword").type("Perro salchicha"); 
         cy.get("#txtpassword").should('have.value','Perro salchicha'); 
       });
     it("Registrar repetir password de usuario", () => {
-        cy.visit("/");
-        cy.get("#menucrearusuario").click();
         cy.get("#txtreppass").type("Perro salchicha"); 
         cy.get("#txtreppass").should('have.value','Perro salchicha'); 
     });
