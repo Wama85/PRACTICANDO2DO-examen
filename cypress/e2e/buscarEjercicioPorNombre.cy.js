0ae6c0c73d9b774dcf2748d78743482d426bb0ff
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-11-11 20:38:47 -0400

    Encuentra los links de las imagenes de las coincidencias del ejercicio buscado

--- cypress/e2e/buscarEjercicioPorNombre.cy.js
+++ cypress/e2e/buscarEjercicioPorNombre.cy.js
@@ -24,5 +24,13 @@ describe("Buscar ejecicio por nombre", () => {
         cy.get("#marcoimagen img").should('be.visible');
   
       });
+      it("Encuentra los links de las imagenes de las coincidencias del ejercicio buscado", () => {
+        cy.visit("/");
+        cy.get("#menubuscarejer").click();
+        cy.get("#txtbuscar").type("Numeros Primos");
+        cy.get("#txtbuscar").type("{enter}");
+        cy.get("#marcoimagen img").should('have.attr','src','https://drive.google.com/uc?export=download&id=1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph');
+  
+      });
   });
 
