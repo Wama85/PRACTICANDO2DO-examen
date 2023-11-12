0b1e1d617de445399b3f05faf67253f1003e6efb
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-11 22:32:01 -0400

    Registrar password de usuario

--- cypress/e2e/crearUsuario.cy.js
+++ cypress/e2e/crearUsuario.cy.js
@@ -6,4 +6,10 @@ describe("Crear usuario", () => {
       cy.get("#txtusername").type("Perro salchicha"); 
       cy.get("#txtusername").should('have.value','Perro salchicha'); 
     });
+    it("Registrar password de usuario", () => {
+        cy.visit("/");
+        cy.get("#menucrearusuario").click();
+        cy.get("#txtpassword").type("Perro salchicha"); 
+        cy.get("#txtpassword").should('have.value','Perro salchicha'); 
+      });
 });
