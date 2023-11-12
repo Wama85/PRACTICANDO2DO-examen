956c8de263638e6d48cd9f607724842ee99ddceb
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-11 22:35:16 -0400

    Registrar repetir password de usuario

--- cypress/e2e/crearUsuario.cy.js
+++ cypress/e2e/crearUsuario.cy.js
@@ -12,4 +12,10 @@ describe("Crear usuario", () => {
         cy.get("#txtpassword").type("Perro salchicha"); 
         cy.get("#txtpassword").should('have.value','Perro salchicha'); 
       });
+    it("Registrar repetir password de usuario", () => {
+        cy.visit("/");
+        cy.get("#menucrearusuario").click();
+        cy.get("#txtreppass").type("Perro salchicha"); 
+        cy.get("#txtreppass").should('have.value','Perro salchicha'); 
+    });
 });
