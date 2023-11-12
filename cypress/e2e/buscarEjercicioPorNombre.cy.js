47aa441998f35cf34c481840d4e9729c06b5b88c
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-11-11 21:27:19 -0400

    Refactoring pruebas

--- cypress/e2e/buscarEjercicioPorNombre.cy.js
+++ cypress/e2e/buscarEjercicioPorNombre.cy.js
@@ -7,45 +7,38 @@ function mostrarImagen(imagenUrl){
   
   return URLIMAGENES+imagenUrl;
 }
+function escribirEjercicioBuscado(titulo){
+    cy.visit("/");
+        cy.get("#menubuscarejer").click();
+        cy.get("#txtbuscar").type(titulo);
+        cy.get("#txtbuscar").type("{enter}");
+}
 describe("Buscar ejecicio por nombre", () => {
+
     it("Encuentra los titulos de las coincidencias del ejercicio buscado", () => {
-      cy.visit("/");
-      cy.get("#menubuscarejer").click();
-      cy.get("#txtbuscar").type("Numeros Primos");
-      cy.get("#txtbuscar").type("{enter}");
+        escribirEjercicioBuscado("Numeros Primos");
       cy.get("#contenidodetallecatalogo h3").should("contain","Numeros Primos");
 
     });
     it("Encuentra los resumenes de las coincidencias del ejercicio buscado", () => {
-        cy.visit("/");
-        cy.get("#menubuscarejer").click();
-        cy.get("#txtbuscar").type("Numeros Primos");
-        cy.get("#txtbuscar").type("{enter}");
+        escribirEjercicioBuscado("Numeros Primos");
         cy.get("#contenidodetallecatalogo p").should("contain",buscarEjercicio("Numeros Primos").getResumen());
   
       });
 
       it("Verifica que la imagen sea visible de las coincidencias del ejercicio buscado", () => {
-        cy.visit("/");
-        cy.get("#menubuscarejer").click();
-        cy.get("#txtbuscar").type("Numeros Primos");
-        cy.get("#txtbuscar").type("{enter}");
+        escribirEjercicioBuscado("Numeros Primos");
+        
         cy.get("#marcoimagen img").should('be.visible');
   
       });
       it("Encuentra los links de las imagenes predeterminadas que coincidan con el ejercicio buscado", () => {
-        cy.visit("/");
-        cy.get("#menubuscarejer").click();
-        cy.get("#txtbuscar").type("Numeros Primos");
-        cy.get("#txtbuscar").type("{enter}");
-        cy.get("#marcoimagen img").should('have.attr','src',mostrarImagen(IMAGENPREDETERMINADA));
-  
-      });
-      it("Encuentra los links de las imagenes de las coincidencias del ejercicio buscado", () => {
-        cy.visit("/");
-        cy.get("#menubuscarejer").click();
-        cy.get("#txtbuscar").type("Numeros Romanos");
-        cy.get("#txtbuscar").type("{enter}");
+        escribirEjercicioBuscado("Numeros Primos");
+          cy.get("#marcoimagen img").should('have.attr','src',mostrarImagen(IMAGENPREDETERMINADA));
+          
+        });
+        it("Encuentra los links de las imagenes de las coincidencias del ejercicio buscado", () => {
+          escribirEjercicioBuscado("Numeros Romanos");
         cy.get("#marcoimagen img").should('have.attr','src',mostrarImagen('10PUZY7ai9akJ5VAGzWbPNKGjQXBQ4-f9'));
   
       });
