8beeb6b6c55b25541a2a85ac90ca138727e95c0d
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-11-11 20:16:17 -0400

    Encuentra los resumenes de las coincidencias del ejercicio buscado

--- cypress/e2e/buscarEjercicioPorNombre.cy.js
+++ cypress/e2e/buscarEjercicioPorNombre.cy.js
@@ -5,7 +5,15 @@ describe("Buscar ejecicio por nombre", () => {
       cy.get("#txtbuscar").type("Numeros Primos");
       cy.get("#txtbuscar").type("{enter}");
       cy.get("#contenidodetallecatalogo h3").should("contain","Numeros Primos");
-      
+
     });
-  });
+    it("Encuentra los resumenes de las coincidencias del ejercicio buscado", () => {
+        cy.visit("/");
+        cy.get("#menubuscarejer").click();
+        cy.get("#txtbuscar").type("Numeros Primos");
+        cy.get("#txtbuscar").type("{enter}");
+        cy.get("#contenidodetallecatalogo p").should("contain","Los números primos son enteros mayores que 1 con solo dos divisores, 1 y ellos mismos. No son divisibles por ningún otro número. Son fundamentales en matemáticas y tienen aplicaciones en criptografía y ciencias de la computación.Ver mas");
   
+      });
+  });
+
