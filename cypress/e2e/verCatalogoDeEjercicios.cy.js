26e43ee01b85e81ee9a9cb78be3567ee13fafd87
987sebasBeller <sebastianbeller76@gmail.com>
2023-11-13 21:07:49 -0400

    Refactoring pruebas se usa un beforeEach

--- cypress/e2e/verCatalogoDeEjercicios.cy.js
+++ cypress/e2e/verCatalogoDeEjercicios.cy.js
@@ -1,22 +1,20 @@
+
+
 describe("Ver catalogo de ejercicio", () => {
-    it("Devuelve el titulo del ejericicio ATDD",()=>{
+    beforeEach(() => {
         cy.visit("/");
-        cy.get("#menucatalogo").click()
+        cy.get("#menucatalogo").click();
+      });
+    it("Devuelve el titulo del ejericicio ATDD",()=>{
         cy.get("#ejercicio-1").should("contain","Numeros Primos")
     });
     it("Devuelve el resumen del ejericicio ATDD",()=>{
-        cy.visit("/");
-        cy.get("#menucatalogo").click()
         cy.get("#ejercicio-1 + p").should("contain","Los números primos son enteros mayores que 1 con solo dos divisores, 1 y ellos mismos. No son divisibles por ningún otro número. Son fundamentales en matemáticas y tienen aplicaciones en criptografía y ciencias de la computación.")
     });
     it("Verifica que la iamgen sea visible el del ejericicio ATDD",()=>{
-        cy.visit("/");
-        cy.get("#menucatalogo").click()
         cy.get("#imagen-ejer1").should("be.visible")
     });
     it("Devuelve el link  del ejericicio ATDD",()=>{
-        cy.visit("/");
-        cy.get("#menucatalogo").click()
         cy.get("#imagen-ejer1").should("have.attr","src","https://drive.google.com/uc?export=download&id=1euc7nmD5AGroYcxYYx4DEd-MYAzXuUph")
     });
   });
