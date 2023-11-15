3b5706c65b9bdabe56c2de5c8eb3491cdb59386a
Wama85 <wi_an9@hotmail.com>
2023-11-14 22:25:23 -0400

    Verificar que el ejercicio se ha creado con éxito

--- cypress/e2e/crearEjercicio.cy.js
+++ cypress/e2e/crearEjercicio.cy.js
@@ -1,4 +1,12 @@
 describe("Crear Ejercicio", () => {
+  it("Verificar que los campos tengan información", () => {
+    cy.visit("/");
+    cy.get("#menuacregarejer").click();
+    cy.get("#crearregistro").click();
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Todos los campos son obligatorios`)
+    })
+  });
 
     it("Registrar un nuevo ejercicio", () => {
       cy.visit("/");
@@ -13,17 +21,24 @@ describe("Crear Ejercicio", () => {
       cy.on('window:alert', (str) => {
         expect(str).to.equal(`Datos subidos correctamente`)
       })
-
-      
     });
-    it("Verificar que los campos tengan información", () => {
+
+    it("Verificar que el ejercicio se ha creado con éxito", () => {
       cy.visit("/");
       cy.get("#menuacregarejer").click();
+      cy.get("#txtNombre").type("Romanos II"); 
+      cy.get("#txtNombre").should('have.value','Romanos II'); 
+      cy.get("#selecCategoria").select("Geometría"); 
+      cy.get("#txtDetalle").type("Perro salchicha"); 
+      cy.get("#txtDetalle").invoke('val').should('contain','Perro salchicha'); 
       cy.get("#crearregistro").click();
-      cy.on('window:alert', (str) => {
-        expect(str).to.equal(`Todos los campos son obligatorios`)
-      })
+    
 
-      
+      cy.get("#menucatalogo").click();
+      cy.get("#contenidodetallecatalogo #ejercicio-12").should('contain','Romanos II'); 
+    
+     
     });
+ 
+
 });
