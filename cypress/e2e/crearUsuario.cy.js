0bd3fed1e16b5cd7e6d14d255ee63878b5792358
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-14 23:22:29 -0400

    Refactoring de pruebas ATDD crear usuario

--- cypress/e2e/crearUsuario.cy.js
+++ cypress/e2e/crearUsuario.cy.js
@@ -1,16 +1,15 @@
 describe("Crear usuario", () => {
-
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get("#menucrearusuario").click();
+  });
     it("Verifica que los campos del usuario esten llenos", () => {
-      cy.visit("/");
-      cy.get("#menucrearusuario").click();
       cy.get("#btncrearuser").click();
       cy.on('window:alert', (str) => {
       expect(str).to.equal(`Todos los campos son obligatorios o la contraseña no es idéntica`)
       })
     });
     it("Muestra mensaje usuario creado exitosamente", () => {
-      cy.visit("/");
-      cy.get("#menucrearusuario").click();
       cy.get("#txtusername").type("Perro salchicha"); 
       cy.get("#txtpassword").type('Perro salchicha'); 
       cy.get("#txtreppass").type("Perro salchicha"); 
