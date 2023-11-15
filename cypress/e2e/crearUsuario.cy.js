a8b917df64ab6a212524923d2bc5964b86a43139
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-11-14 23:16:30 -0400

    Muestra mensaje usuario creado exitosamente

--- cypress/e2e/crearUsuario.cy.js
+++ cypress/e2e/crearUsuario.cy.js
@@ -8,5 +8,16 @@ describe("Crear usuario", () => {
       expect(str).to.equal(`Todos los campos son obligatorios o la contraseña no es idéntica`)
       })
     });
+    it("Muestra mensaje usuario creado exitosamente", () => {
+      cy.visit("/");
+      cy.get("#menucrearusuario").click();
+      cy.get("#txtusername").type("Perro salchicha"); 
+      cy.get("#txtpassword").type('Perro salchicha'); 
+      cy.get("#txtreppass").type("Perro salchicha"); 
+      cy.get("#btncrearuser").click();
+      cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Usuario creado con éxitosamente`)
+      })
+    });
 
 });
