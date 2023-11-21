d4e2fbe93f53228cca1ba130a5e2673544d511e2
Susan Cespedes <susan.cespedes@ucb.edu.bo>
2023-11-21 00:52:16 -0400

    Refactoring pruebas buscarEjerciciosPorDificultad

--- src/spec/buscarPorDificultad.spec.js
+++ src/spec/buscarPorDificultad.spec.js
@@ -5,6 +5,7 @@ import { Ejercicio } from "../models/Ejercicio";
 function getDificultad(ejercicio){
     return ejercicio.getDificultad();
 }
+
 describe("Dificultad de ejercicios", () => {
   it("Devuelve la dificultad BAJA del ejercicio", () => {
     expect(getDificultad(ejerciciosPruebas[1])).toEqual("BAJA");
@@ -15,20 +16,22 @@ describe("Dificultad de ejercicios", () => {
 }
 );
 
+function buscarEjerciosPorDificultad(ejercicios,dificultad){
+  let catalogo=new Catalogo(ejercicios);
+  return catalogo.buscarEjerciosPorDificultad(dificultad);
+}
+
 describe("Buscar ejercicios por dificultad", () => {
     it("Devuelve la lista vacia sin coincidencias de dificultad", () => {
-      let catalogo=new Catalogo();
-      expect(catalogo.buscarEjerciosPorDificultad()).toEqual([]);
+      expect(buscarEjerciosPorDificultad()).toEqual([]);
     }); 
 
     it("Devuelve una coincidencia de ejercicio por dificultad ALTA", () => {
-      let catalogo=new Catalogo([ejerciciosPruebas[0]]);
-      expect(catalogo.buscarEjerciosPorDificultad("ALTA")).toEqual([ejerciciosPruebas[0]]);
+      expect(buscarEjerciosPorDificultad([ejerciciosPruebas[0]],"ALTA")).toEqual([ejerciciosPruebas[0]]);
     });
 
     it("Devuelve una coincidencia de ejercicio por dificultad MEDIA", () => {
-    let catalogo=new Catalogo(ejerciciosPruebas);
-    expect(catalogo.buscarEjerciosPorDificultad("MEDIA")).toEqual(ejerciciosPruebas.filter(ejer=>ejer.getDificultad()=="MEDIA"));
+    expect(buscarEjerciosPorDificultad(ejerciciosPruebas,"MEDIA")).toEqual(ejerciciosPruebas.filter(ejer=>ejer.getDificultad()=="MEDIA"));
   });
  
   }
