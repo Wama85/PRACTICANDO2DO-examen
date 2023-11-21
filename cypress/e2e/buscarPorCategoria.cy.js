3f61944f5dfe62a6ed1d70b8a0e3d6bb1c52e2e6
Wama85 <wi_an9@hotmail.com>
2023-11-20 23:57:56 -0400

    Refactorizamos la cabecera de las pruebas

--- cypress/e2e/buscarPorCategoria.cy.js
+++ cypress/e2e/buscarPorCategoria.cy.js
@@ -2,27 +2,30 @@ import { ejercicios } from "../../src/dataCatalogo";
 import { URLIMAGENES, IMAGENPREDETERMINADA } from "../../src/constantes";
 
 describe("Buscar ejecicio por categoria", () => {
-
-    it("Devuelve el titulo del encabezado buscar por categoria", () => {
+  beforeEach(() => {
     cy.visit("/");
     cy.get("#menubuscarejerPorCategoria").click();
-    cy.get("#titulocatalogo").should("contain", "Buscar Ejercicios por Categoria")
+  });
+
+  
+    it("Devuelve el titulo del encabezado buscar por categoria", () => {
+   
+      cy.get("#titulocatalogo").should("contain", "Buscar Ejercicios por Categoria")
 
     });
     it("Devuelve el titulo del ejercicio buscado por categoria", () => {
-      cy.visit("/");
-      cy.get("#menubuscarejerPorCategoria").click();
-      cy.get("#seleccionarcategoria").select("Numeros")
+ 
+       cy.get("#seleccionarcategoria").select("Numeros")
       cy.get("#contenidodetallecatalogo h3").should("contain", "Numeros Primos")
       
       });
       it("Devuelve el titulo del ejercicio buscado por categoria cadena", () => {
-        cy.visit("/");
-        cy.get("#menubuscarejerPorCategoria").click();
+      
         cy.get("#seleccionarcategoria").select("Cadenas")
         cy.get("#contenidodetallecatalogo h3").first().should("contain", "Inversión de cadena")
         
         });
+    
 
   });
 
